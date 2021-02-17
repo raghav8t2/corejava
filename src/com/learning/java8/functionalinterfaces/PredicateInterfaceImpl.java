d8dc6558b52fb271e5f6dcc10e2cf7cdc40e9312
raghav8t2 <raghav.telgar@gmail.com>
2021-02-17 21:42:19 +0530

    predicate examples

--- src/com/learning/java8/functionalinterfaces/PredicateInterfaceImpl.java
+++ src/com/learning/java8/functionalinterfaces/PredicateInterfaceImpl.java
@@ -1,10 +1,46 @@
 package com.learning.java8.functionalinterfaces;
 
+import java.util.List;
+import java.util.function.Predicate;
+
+import com.learning.db.StudentDB;
+import com.learning.pojo.Student;
+
 public class PredicateInterfaceImpl {
 
+	static List<Student> allStudents = StudentDB.getStudents();
+	
+	static Predicate<Student> p1 = (student) -> {
+		return student.getGpa() >= 3;
+	};
+	
+	static Predicate<Student> p2 = (student) -> {
+		return student.getGrade() == 2;
+	};
+	
+	public static void getStudentsWithGpaAbove3() {
+		allStudents.forEach((s) -> {
+			if(p1.test(s)) {
+				System.out.println(s.toString());
+			};
+		});
+	}
+	
+	public static void getStudentsOfGrade2AndGpaAbove3() {
+		allStudents.forEach((s) -> {
+			if(p1.and(p2).test(s)) {
+				System.out.println(s.toString());
+			};
+		});
+	}
+		
+		
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Predicate Example1: Get students with GPA above 3");
+		getStudentsWithGpaAbove3();
+		System.out.println(" ");
+		System.out.println("Predicate Example2: Get Grade2students with GPA above 3");
+		getStudentsOfGrade2AndGpaAbove3();
 	}
 
 }
